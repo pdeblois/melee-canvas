cd786b2eb4548ab3a46ee8dfdba4a923f5700851
Philippe De Blois <philippe2blois@gmail.com>
2024-01-25 23:45:22 -0500

    Add graphviz option to build script

--- build.py
+++ build.py
@@ -10,13 +10,15 @@ def run_cmd(cmd):
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--generator", type=str, help="CMake generator")
+parser.add_argument("--generator", type=str, default="", help="CMake generator")
 
 parser.add_argument("--variant", type=str, default="RelWithDebInfo", help="CMake build variant")
 
 parser.add_argument("--cmake-vars", type=str, nargs="*", default=[], help="CMake options and variables")
 
 parser.add_argument("--steps", type=str, nargs="*", default=['clean','configure','build'], help="Build steps to run")
+
+parser.add_argument("--graphviz-file", type=str, default="", help="Graphviz output file")
 
 args = parser.parse_args()
 
@@ -36,10 +38,12 @@ if 'configure' in args.steps:
             "-G {generator} "
             "-DCMAKE_BUILD_TYPE={variant} "
             "{cmake_args} "
+            "{graphviz_arg} "
             .format(source_dir=source_dir, build_dir=build_dir,
                     generator="\"{}\"".format((args.generator or "")),
                     variant=args.variant,
-                    cmake_args=" ".join(["-D" + var for var in args.cmake_vars])))
+                    cmake_args=" ".join(["-D" + var for var in args.cmake_vars]),
+                    graphviz_arg="--graphviz={}".format(args.graphviz_file) if args.graphviz_file else ""))
 
 if 'build' in args.steps:
     run_cmd("cmake --build {build_dir} "
